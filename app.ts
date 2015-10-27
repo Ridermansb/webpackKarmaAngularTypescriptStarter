9a04e3cefabfdd766300af6e41ec7bb873de5658
ridermanb <ridermanb@takenet.com.br>
2015-10-27 11:32:16 -0200

    Add module dependency to typescript

--- app.ts
+++ app.ts
@@ -1,3 +1,14 @@
-/**
- * Created by ridermanb on 27/10/2015.
- */
+export default class Calc {
+
+    public Display : number;
+
+    constructor(display : number = 0) {
+        this.Display = display;
+    }
+
+    sum(num : number) : number {
+        this.Display += num;
+        return this.Display;
+    }
+
+}
--- test/app-test.ts
+++ test/app-test.ts
@@ -1,5 +1,23 @@
+import Calc from "../app";
+
 describe('test', () => {
     it('should work', () => {
         true.should.be.true;
+    });
+});
+
+describe('calc', () => {
+    it('Calc object should be defined', () => {
+       Calc.should.be.not.undefined;
+    });
+
+    it('calc instance should be defined', () => {
+        var calc = new Calc();
+        calc.should.be.not.undefined;
+    });
+
+    it('sum 1 should return 1', () => {
+        var calc = new Calc();
+        calc.sum(1).should.be.equal(1);
     });
 });
